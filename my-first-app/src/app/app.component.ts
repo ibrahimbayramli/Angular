d494de8e1f3bacfb255ae814e63b9dd1c74c6494
ibrahimbayramli <ibrahimbayramli1902@gmail.com>
2022-09-05 21:50:49 +0300

    Home component creating.

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-app';
+
+  krediTutari: number = 10000;
+  vade: number = 36;
+  aylikOdemeniz: number = 0;
+  aylikFaizTutari: number = 117.83;
+  toplamGeriOdemeniz: number = 0;
+
+  hesapla() {
+    this.aylikOdemeniz = (this.krediTutari / this.vade) + this.aylikFaizTutari;
+    this.toplamGeriOdemeniz = this.aylikOdemeniz * this.vade
+
+  }
+
+
 }
